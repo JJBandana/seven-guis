cf46479613961cbb83c1e1d259f8b0e13dda0af3
Juanjo <jj.briasco@gmail.com>
2024-11-02 16:41:58 +0100

    finished counter and temperature

---
+++ src/components/CounterComponent.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  let count = $state(0)
+</script>
+
+<div class="counter">
+  <input type="number" bind:value={count} disbaled />
+  <button onclick={() => count++}>Count</button>
+</div>
---
+++ src/components/TemperatureConverter.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  class Temperature {
+    #c = $state(0)
+    #f = $state(32)
+
+    get c(){
+      return this.#c
+    }
+
+    get f(){
+      return this.#f
+    }
+
+    set c(c){
+      this.#c = c
+      this.#f = c * (9 / 5) + 32
+    }
+
+    set f(f){
+      this.#f = f
+      this.#c = (f - 32) * (5 / 9)
+    }
+  }
+
+  const temperature = new Temperature()
+</script>
+
+<div class="temperature">
+  <input type="number" bind:value={temperature.c}/> Celcius =
+  <input type="number" bind:value={temperature.f}/> Fahrenheit
+</div>
